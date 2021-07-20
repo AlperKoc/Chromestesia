2adad2525822125b82b5a0365245404c2affebcf
Alper Koc <alperkoc@Alpers-MacBook-Pro.local>
2021-07-20 11:56:31 +0100

    config

--- Barney.py
+++ Barney.py
@@ -3,6 +3,8 @@ import math
 
 class Barney():
     def __init__(self,config,time):
+        self.count       = 0                     # int - total frame count of barney
+        self.step        = 0
         self.x           = config['x']           # int [0,width]
         self.y           = config['y']           # int [0,height]
         self.z           = config['z']           # for later
@@ -10,24 +12,21 @@ class Barney():
         self.direction_y = config['direction_y'] # bool
         self.rand_x      = config['rand_x']      # int
         self.rand_y      = config['rand_y']      # int
-        self.fill        = config['fill']        # (H,S,B)
-        self.stroke      = config['stroke']      # (H,S,B)
-        self.count       = 0                     # int - total frame count of barney
+        self.rand_global = config['rand_global']  # int [0,10]
+        self.rand_local  = config['rand_local'] # int [0,10]
         self.angle       = config['angle']       # int
-        self.size        = config['size']        # int [0,10]
         self.speed       = config['speed']       # int [0,10]
+        self.step_size   = height/ config['step_size'] # int [0,10]
         self.fade_speed  = config['fade_speed']  # int [0,10]
-        self.local_rand  = config['local_rand']  # int [0,10]
-        self.global_rand = config['global_rand'] # int [0,10]
-        self.bpm         = config['bpm']         # bpm of track
         self.beat_speed  = config['beat_speed']  # float 0.25, 2, 8, 0.50 etc.
-        self.shape       = config['shape']       # 'point' shape of barney
         self.centralize  = config['centralize']  # [0,1] 0 to edges, 1 to center, 0.5 freewalk
         self.instrument  = config['instrument']
+        self.walk_style  = config['walk_style']
+        self.fill        = config['fill']        # (H,S,B)
+        self.stroke      = config['stroke']      # (H,S,B)
+        self.size        = config['size']        # int [0,10]
+        self.shape       = config['shape']       # 'point' shape of barney
         self.time        = time
-        
-     
-        
         
     def get_x(self):
         return self.x
@@ -39,11 +38,15 @@ class Barney():
         return self.direction_y
     def get_count(self):
         return self.count
-    def get_loc(self):
-        mid_x = width/2
-        mid_y = height/2
-        return self.get_x(), self.get_y(), mid_x, mid_y
-    
+    def get_locs(self):
+        mid_x = width/2; mid_y = height/2
+        return self.get_x(), self.get_y(), self.get_direction_x(), self.get_direction_y(), mid_x, mid_y
+    def get_rands(self):
+        return self.rand_x,self.rand_y,self.rand_local,self.rand_global
+    def get_colors(self):
+        fill = self.fill
+        stroke = self.stroke
+        return fill,stroke
     def set_x(self,x):
         self.x = x
     def set_y(self,y):
@@ -58,10 +61,11 @@ class Barney():
         self.rand_y = rand_y
     def count_up(self):
         self.count +=1
+    def step_up(self):
+        self.step +=1
     
     def jail(self):
-        x = self.get_x()
-        y = self.get_y()
+        x,y,direction_x,direction_y,mid_x,mid_y = self.get_locs()
         
         if x <= 0:
             self.set_direction_x(True)
@@ -80,79 +84,91 @@ class Barney():
             self.set_rand_x(random.uniform(0, 1))
             self.set_rand_y(random.uniform(0, 1))
             
-    def random_machine(self):
-        direction_x = self.get_direction_x()
-        direction_y = self.get_direction_y()
-        x = self.get_x()
-        y = self.get_y()
+    def random_walk(self):
+        x,y,direction_x,direction_y,mid_x,mid_y = self.get_locs()
         
+        c = 40
+        r = 9
         if direction_x:
-            x += 1 * self.rand_x + random.uniform(-3, 3)
+            x += c * self.rand_x + random.uniform(-r, r)
             self.set_x(x)
         else:
-            x -= 1 * self.rand_x + random.uniform(-3, 3)
+            x -= c * self.rand_x + random.uniform(-r, r)
+            
+            
             self.set_x(x)
         if direction_y:
-            y += 1 * self.rand_y + random.uniform(-3, 3)
+            y += c * self.rand_y + random.uniform(-r, r)
             self.set_y(y)
         else: 
-            y -= 1 * self.rand_y + random.uniform(-3, 3)
+            y -= c * self.rand_y + random.uniform(-r, r)
             self.set_y(y)
             
-        if self.get_count() % 40 == 0:
+        if self.get_count() % 200 == 0:
             self.set_direction_x(random.choice([True, False]))
             self.set_direction_y(random.choice([True, False]))
             self.set_rand_x(random.uniform(0, 1))
             self.set_rand_y(random.uniform(0, 1))
-
-    def update(self):        
+            
+    def square_walk(self):
+        x,y,direction_x,direction_y,mid_x,mid_y = self.get_locs()
+        # direction = self.direction
+        
+        # if direction == 'right':
+        #         direction = 'down'
+        # elif direction == 'down':
+        #         direction = 'left'
+        # elif direction == 'left':
+        #         direction = 'up'    
+        # elif direction == 'up':
+        #         direction = 'right'
+    def check_turn(self):
         self.count_up() # count += 1
-        self.jail() # check edges and keep it inside
-        self.random_machine() # add local random movements
-        
-    def walk(self, _fill, _stroke, speed, evolve_speed, beat_speed, angle):
-        x,y,mid_x,mid_y = self.get_loc()
-
-        colorMode(HSB, 100)
-        fill  (*self.fill)
-        stroke(*self.stroke)
-        
-        if beat_speed:
-            circle(x,y,30)
-        else:
-            stroke(90,60,50)
-            circle(x,y,10)
-        
-    def splash(self, fill, stroke, speed, evolve_speed, beat_speed):
-        pass
-        # fill(count%120+54 + count%134+98 ,count%200+34)
-        # stroke(12,108,231,10)
-        # circle(x,y,count%56+1)
-        # if barnies[0].count % 10 == 0:
-        #     line(mid_x, mid_y, x, y)
-        # stroke(count%20+100 + count%20+20 ,count%50+200)
-        
-        # if count == 100: 
-        #   start_x = random.randint(0,width)
-        #   start_y = random.randint(0,height)
-
-        #   barney2 = Barney(start_x,start_y,True,True,1,1,0,0)
-        #   barnies.append(barney2)
-        
-    def is_it_my_turn(self):
         time = self.time
         beat_speed = self.beat_speed
         return time.check_beat(beat_speed)
+        
+    def smooth(self):
+        x,y,direction_x,direction_y,mid_x,mid_y = self.get_locs()
+        c = self.count
+        fill(c%120+54 + c%134+98 ,c%200+34)
+        stroke(12,108,231,10)
+        circle(x,y,c%56+1)
+        if c % 10 == 0:
+            line(mid_x, mid_y, x, y)
+        stroke(c%20+100 + c%20+20 ,c%50+200)
     
+    def simple(self):
+        x,y,direction_x,direction_y,mid_x,mid_y = self.get_locs()
+        stroke(90,60,50)
+        circle(x,y,10)
+
+    
+    def render(self):
+        self.step_up()
+        shape = self.shape
+        fill_, stroke_ = self.get_colors()
+       
+        colorMode(HSB, 100)
+        fill  (*fill_)
+        stroke(*stroke_)
+        
+        if self.shape == 'simple':
+            self.simple()
+            
+        if self.shape == 'smooth':
+            self.smooth()
+            
+    def update(self):        
+        self.jail() # check edges and keep it inside
+        if self.walk_style == 'random':
+            self.random_walk() # add local random movements
+        if self.walk_style == 'square':
+            self.square_walk() # add local random movements
+
     def draw(self):
-        if self.is_it_my_turn():
-            print(self.beat_speed)
-            self.walk(self.fill,
-                self.stroke,
-                self.speed,
-                self.fade_speed ,
-                self.beat_speed,
-                self.angle)
+        if self.check_turn():
+            self.render()
 
 
         
--- Chromesthesia.pyde
+++ Chromesthesia.pyde
@@ -2,10 +2,10 @@ import random
 import math
 from Barney import Barney
 from Time import Time
-
+ 
 barnies = []
 beats = []
-time = Time(133,60)
+time = Time(112,60)
 
 def setup():
     size(700,700)
@@ -17,26 +17,30 @@ def setup():
     beats = [1]
     
     
-    barney_config = {"x":0,"y":0,"z":0,
+    barney_config = {"x":0,"y":10,"z":0,
                      "direction_x": True,
-                     "direction_y": True,
+                     "direction_y": False,
                      "rand_x":0, "rand_y":0,
-                     "fill":(128,128,128),"stroke":(128,128,128),
-                     "angle":0, "size": 1, "speed":1, "fade_speed":1,
-                     "local_rand": 0, "global_rand": 0,
-                     "bpm":120, "beat_speed":4,"shape":"circle", 
-                     "centralize":0.5,'instrument':'bassline'}
+                     "rand_local": 0, "rand_global": 0,
+                     "angle":0, "speed":1, "step_size":16,
+                     "fade_speed":1,"beat_speed":1,
+                     "centralize":0.5,'instrument':'bassline',
+                     "walk_style": "random",
+                     "fill":(128,128,128),"stroke":(255,255,255),
+                     "size": 1, "shape":"simple"}
     
-    barney_config2 = {"x":0,"y":0,"z":0,
+    barney_config2 = {"x":0,"y":0,"z":0,'direction':'right',
                      "direction_x": True,
                      "direction_y": True,
                      "rand_x":0, "rand_y":0,
+                     "rand_local": 0, "rand_global": 0,
+                     "angle":0, "speed":1,"step_size":10,
+                     "fade_speed":1,"beat_speed":0.5,
+                     "centralize":0.5,'instrument':'bassline',
+                     "walk_style": "random",
                      "fill":(22,22,22),"stroke":(222,222,222),
-                     "angle":0, "size": 1, "speed":1, "fade_speed":1,
-                     "local_rand": 0, "global_rand": 0,
-                     "bpm":120, "beat_speed":0.5, "shape":"circle", 
-                     "centralize":0.5,'instrument':'bassline'}
-    
+                     "size": 1, "shape":"smooth"}
+
     barney  = Barney(barney_config,time)#(0,0,True,True,1,1,0,0,0.25)
     barney2 = Barney(barney_config2,time)#(0,0,True,True,1,1,0,0,0.25)
 
@@ -52,8 +56,10 @@ def draw():
     
 
     for barney in barnies:
-        barney.update()
-        barney.draw()
+        if barney.check_turn():
+            print(barney.walk_style)
+            barney.update()
+            barney.draw()
 
 
 
